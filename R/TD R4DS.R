3e086234d085d8ec2d53dc49bd105a0240824b1e
Cynism <axelfg@sfr.fr>
2020-04-28 09:49:38 +0200

    Edition et tests Import R

--- R/TD R4DS.R
+++ R/TD R4DS.R
@@ -1,4 +1,8 @@
 setwd("C:/Users/axelf/Desktop/Codage/OPEN/GitHub/RStudioOPEN/R")
 
+install.packages("tidyverse")
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
 library(nycflights13)
 library(tidyverse)
+fly <- nycflights13::flights;fly

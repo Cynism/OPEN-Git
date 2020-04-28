dde0f83db0f26133b084a9364b40c135fc0df4d6
Cynism <axelfg@sfr.fr>
2020-04-28 10:00:26 +0200

    Edit 2

--- R/TD R4DS.R
+++ R/TD R4DS.R
@@ -1,8 +1,14 @@
 setwd("C:/Users/axelf/Desktop/Codage/OPEN/GitHub/RStudioOPEN/R")
 
-install.packages("tidyverse")
-install.packages(c("nycflights13", "gapminder", "Lahman"))
+#install.packages("tidyverse")
+#install.packages(c("nycflights13", "gapminder", "Lahman"))
 
 library(nycflights13)
 library(tidyverse)
 fly <- nycflights13::flights;fly
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl = 8)
+filter(diamond, carat > 3)
